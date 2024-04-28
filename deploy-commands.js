cc78b7a4bead5b4e11245fd33e0d28cbe98d62c0
Joshua <94143587+JoshuaO22@users.noreply.github.com>
2024-04-28 02:53:32 -0400

    add comment

--- deploy-commands.js
+++ deploy-commands.js
@@ -33,6 +33,7 @@ const rest = new REST().setToken(token);
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
+		// TODO: figure out a way to deploy commands globally
 		const data = await rest.put(
 			Routes.applicationGuildCommands(clientId, guildId),
 			{ body: commands },
